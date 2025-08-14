b30b99459d4007782713430d2795e1aa8ebab74a
KyleUndo <ando.kyle404@gmail.com>
2025-08-14 09:20:24 +0800

    adds

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,15 +5,17 @@ function ProductCard(props) {
         <img src="./buy.gif" alt="" />
       </div>
       <img className="card-img" src={props.image} alt="alt" />
-      <h2>{props.name}</h2>
-      <div className="description">
-        <p className="description">{props.description}</p>
-      </div>
-      <span>${props.price}</span>
-      <div className="add-to-cart-button-container">
-        <button onClick={() => console.log("Product name: ", props.name)}>
-          Buy Now
-        </button>
+      <div className="description-container">
+        <h2>{props.name}</h2>
+        <div className="description">
+          <p className="description">{props.description}</p>
+        </div>
+        <span>${props.price}</span>
+        <div className="add-to-cart-button-container">
+          <button onClick={() => console.log("Product name: ", props.name)}>
+            Buy Now
+          </button>
+        </div>
       </div>
     </div>
   );
